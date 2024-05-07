0ec72841b6582d81b82483dcfccd6c26d9de605e
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-07 14:00:17 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets_102/pages/counter_page.dart';
+import 'package:flutter_widgets_102/pages/todo_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoPage(),
+    );
+  }
+}
