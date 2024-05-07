d456641f828ebbbd89f143bd637ffe317cf9090c
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-07 13:39:31 +0300

    todo app main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets_102/pages/counter_page.dart';
+import 'package:flutter_widgets_102/pages/todo_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterPage(),
+      home: TodoPage(),
     );
   }
 }
