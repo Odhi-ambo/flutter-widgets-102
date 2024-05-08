5731c055c8ce97a13cd8afb6356b96973b0782a3
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-08 18:01:08 +0300

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets_102/pages/home_page.dart';
-import 'package:flutter_widgets_102/pages/todo_page.dart';
 
 void main() {
   runApp(const MyApp());
