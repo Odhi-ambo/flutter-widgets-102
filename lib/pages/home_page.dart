c510c9a59598a4de1eba61468ddc3ac30e5c02a8
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-10 09:04:40 +0300

    modified

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets_102/utils/dialog_box.dart';
 import 'package:flutter_widgets_102/utils/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,6 +21,14 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void createNewTask() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return DialogBox();
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +37,7 @@ class _HomePageState extends State<HomePage> {
         title: Center(child: Text('To Do')),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: createNewTask,
         child: Icon(Icons.add),
       ),
       body: ListView.builder(
