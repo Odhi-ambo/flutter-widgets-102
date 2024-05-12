fdc07e4704a46f6b0c097a5bad4f31218356baba
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-12 09:19:38 +0300

    modified

--- lib/utils/dialog_box.dart
+++ lib/utils/dialog_box.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widgets_102/utils/my_button.dart';
 
 class DialogBox extends StatelessWidget {
-  const DialogBox({super.key});
+  final controller;
+  const DialogBox({
+    super.key,
+    required this.controller,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +18,7 @@ class DialogBox extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextField(
+              controller: controller,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Add a new task',
@@ -23,6 +28,9 @@ class DialogBox extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 MyButton(text: 'Save', onPressed: () {}),
+                const SizedBox(
+                  width: 4,
+                ),
                 MyButton(text: 'Cancel', onPressed: () {})
               ],
             )
