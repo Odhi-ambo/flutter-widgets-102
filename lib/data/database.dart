827a93e5f0c2bb4b2b94ce3a86db59c43ce4bb1f
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-13 15:03:31 +0300

    initialising hive db

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -2,5 +2,20 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 class ToDoDatabase {
   List toDoList = [];
-  final _myBox = Hive.openBox('mybox');
+  final _myBox = Hive.box('mybox');
+
+  void createInitialData() {
+    toDoList = [
+      ['Go to the gym', false],
+      ['Do some coding', false],
+    ];
+  }
+
+  void loadData() {
+    toDoList = _myBox.get("toDoList");
+  }
+
+  void updateDatabase() {
+    _myBox.put('toDoList', toDoList);
+  }
 }
